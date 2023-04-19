663adf74dd81e11697a6830b1ee009d7a4e7cb4b
mbkranz <kranz-michael@norc.org>
2023-04-19 12:57:16 -0500

    fix: added charset_normalizer dependency

--- setup.py
+++ setup.py
@@ -20,7 +20,8 @@ setup(
         #'dataforge[redcap] @ git+https://gitlab.com/mbkranz/data-forge.git@0afa429d6b7d1f1ec04ff8c4ee127291b3b058d4',
         #NOTE: temporarily taking out dataforge[redcap] as the xml transformations are WIP and to reduce external dependencies for POC
         'dataforge@ git+https://gitlab.com/mbkranz/data-forge.git@0afa429d6b7d1f1ec04ff8c4ee127291b3b058d4',
-        'xmltodict' #NOTE:used until schemas put into dataforge
+        'xmltodict', #NOTE:used until schemas put into dataforge,
+        'charset_normalizer==2.1'
     ],
     entry_points='''
         [console_scripts]
