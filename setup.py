11567aaf9d8560aca82db0c02cb89544bf8ae4f5
mbkranz <kranz-michael@norc.org>
2023-04-20 11:29:33 -0500

    bugfix

--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
 from setuptools import setup, find_namespace_packages
+from pathlib import Path
 
 def generate_long_description():
     return Path("README.md").read_text()
-    
+
+
 setup(
     name='healdata_utils',
     version='0.0.2',
     author='Michael Kranz',
     author_email='kranz-michael@norc.org',
+    long_description=generate_long_description(),
+    long_description_content_type="text/markdown",    
     description='Data packaging tools for the HEAL data ecosystem',
     #TODO: change url to HEAL once migrated.
     url='https://github.com/norc-heal/healdata-utils/src/healdata-utils',
@@ -29,8 +33,6 @@ setup(
     entry_points='''
         [console_scripts]
         vlmd=healdata_utils.cli:main
-    ''',
-    long_description=generate_long_description(),
-    long_description_content_type="text/markdown",
+    '''
 
 )
