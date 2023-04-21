75670a8c89ac15889c035137219fefe3a852f2e4
mbkranz <kranz-michael@norc.org>
2023-04-21 11:14:38 -0500

    Update version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def generate_long_description():
 
 setup(
     name='healdata_utils',
-    version='0.0.2',
+    version='0.0.3',
     author='Michael Kranz',
     author_email='kranz-michael@norc.org',
     long_description=generate_long_description(),
