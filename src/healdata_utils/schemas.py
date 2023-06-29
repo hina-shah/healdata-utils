fa359e389177b9ff4383a50f528342ecca1517ea
mbkranz <kranz-michael@norc.org>
2023-06-29 15:49:47 -0500

    Call jsonschema data-dictionary object directly  rather than fields

--- src/healdata_utils/schemas.py
+++ src/healdata_utils/schemas.py
@@ -1,32 +1,18 @@
 import requests
 
 jsonschema_url = (
-"https://raw.githubusercontent.com/norc-heal/"
-"heal-metadata-schemas/mbkranz/variable-lvl-dev/"
-"variable-level-metadata-schema/schemas/jsonschema/fields.json"
+    "https://raw.githubusercontent.com/norc-heal/"
+    "heal-metadata-schemas/mbkranz/variable-lvl-dev/"
+    "variable-level-metadata-schema/schemas/jsonschema"
 )
 csvschema_url = (
-        "https://raw.githubusercontent.com/norc-heal/heal-metadata-schemas/"
-        "mbkranz/variable-lvl-dev/"
-        "variable-level-metadata-schema/schemas/frictionless/csvtemplate/fields.json"
-    )
+    "https://raw.githubusercontent.com/norc-heal/heal-metadata-schemas/"
+    "mbkranz/variable-lvl-dev/"
+    "variable-level-metadata-schema/schemas/frictionless/csvtemplate/fields.json"
+)
 
-# TODO: currently using fields.json and hardcoding -- use data_dictionaries.json 
-healjsonschema = {
-    'type':'object',
-    'required':[
-        'title',
-        'data_dictionary'
-    ],
-    'properties':{
-        'title':{'type':'string'},
-        'description':{'type':'string'},
-        'data_dictionary':{
-            'type':'array',
-            'items':requests.get(jsonschema_url).json()}
-    }
-}
-healcsvschema = requests.get(csvschema_url).json()   
-
-
-
+# NOTE: replaces relative fields.json with fields 
+# TODO: install schemas from pip or make it a submodule (so no GET call necessary for fields.json)
+healjsonschema = requests.get(jsonschema_url+"/data-dictionary.json").json()
+healjsonschema["properties"]["data_dictionary"]["items"] = requests.get(jsonschema_url+"/fields.json").json()
+healcsvschema = requests.get(csvschema_url).json()
