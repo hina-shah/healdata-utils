06c0211b2422ed9c3d11247263f72b9fcb8c9f62
Michael Kranz <mbkranz@gmail.com>
2023-07-12 16:47:39 -0500

    Updated requirements files etc

--- setup.py
+++ setup.py
@@ -4,6 +4,9 @@ from pathlib import Path
 def generate_long_description():
     return Path("README.md").read_text()
 
+def get_install_requirements():
+    return Path("requirements.txt").read_text()
+
 
 setup(
     name='healdata_utils',
@@ -17,17 +20,7 @@ setup(
     url='https://github.com/norc-heal/healdata-utils',
     package_dir={'': 'src'},
     packages=find_namespace_packages(where='src'),
-    install_requires=[
-        'petl==1.7.12',
-        'jsonschema==4.17.3',
-        'requests==2.28.2',
-        'PyYaml==6.0',
-        #'frictionless==4.40.8',
-        'pyreadstat==1.2.0',
-        'charset_normalizer==2.1',
-        'visions== 0.7.5',
-        "click==8.1.3"
-    ],
+    install_requires=get_install_requirements(),
     entry_points='''
         [console_scripts]
         vlmd=healdata_utils.cli:main
