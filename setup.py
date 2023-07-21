a54cfb8f3b095c61dffc79bbe4fb7d6a76df2118
Michael Kranz <mbkranz@gmail.com>
2023-07-21 08:40:22 -0500

    version update

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ click==8.1.3'''
 
 setup(
     name='healdata_utils',
-    version='0.1.3-alpha',
+    version='0.1.4-alpha',
     author='Michael Kranz',
     author_email='kranz-michael@norc.org',
     long_description=generate_long_description(),
