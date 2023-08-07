edf66f88e3d889ff6526185ac5e1686448b62022
Michael Kranz <mbkranz@gmail.com>
2023-08-07 14:01:03 -0500

    Update frictionless schema for testing

--- tests/transforms/test_frictionless.py
+++ tests/transforms/test_frictionless.py
@@ -1,17 +1,15 @@
 from healdata_utils.transforms.frictionless import convert_frictionless_to_jsonschema
+from healdata_utils.schemas.frictionless import healcsvschema
 from pathlib import Path
 import yaml
 import json
 
-
+#%%
 def test_convert_frictionless_to_jsonschema():
-    with Path(__file__).parent.joinpath("../data/test_frictionless_schema.yaml").open(
-        mode="r"
-    ) as f:
-        frictionless_schema = yaml.safe_load(f)
-    jsonschema_props = convert_frictionless_to_jsonschema(frictionless_schema)
+    frictionless_input_schema = healcsvschema
+    jsonschema_props = convert_frictionless_to_jsonschema(frictionless_input_schema)
     with open("tests/criteria_data/test_frictionless_check1.json", "r") as fp:
         jsonschema_props_check = json.load(fp)
     test_passed = jsonschema_props == jsonschema_props_check
-    if not test_passed:
-        raise Exception("assertion statement/comparison failed")
+
+    assert test_passed,"Assertion statement for conversion from frictonless to jsonschema failed"
