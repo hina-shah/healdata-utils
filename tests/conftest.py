a885bd76c99a385ed208b3cd7f706f10b3702d60
Michael Kranz <mbkranz@gmail.com>
2023-08-15 14:01:10 -0500

    Tests for CSV data inference

--- tests/conftest.py
+++ tests/conftest.py
@@ -19,8 +19,12 @@ def valid_input_params():
     }
     get_input_params = lambda input_filepath: {"filepath":inputdir.joinpath(input_filepath),"data_dictionary_props":data_dictionary_props}
     input_params = {
+        "data.csv":{
+            **get_input_params("data_csv_dataset1.data.csv"),
+            "inputtype":"data.csv"
+        },
         "sas7bdat":{**get_input_params("sas-nmhss-2019/data.sas7bdat"),
-            **{"sas7bcat_filepath":inputdir.joinpath("sas-nmhss-2019/formats.sas7bcat")}
+            "sas7bcat_filepath":inputdir.joinpath("sas-nmhss-2019/formats.sas7bcat")
         }, #TODO: reduce data so easier to manage and test
         "dta":get_input_params("stata_dta_dataset1.dta"),
         # "sav":get_input_params("../../JCOIN_NORC_Omnibus_SURVEY3_June2020.sav"),
@@ -34,6 +38,7 @@ def valid_input_params():
 def valid_output_json():
     path = Path("data/valid/output")
     filenames = {
+        "data.csv":"heal_dd_from_csv_data_dataset1.json",
         "sas7bdat":"heal_dd_from_sas7bdat_with_sas7bcat.json", #TODO: reduce data so easier to manage and test
         "dta":"heal_dd_from_stata_dta_dataset1.json",
         "sav":"heal_dd_from_spss_sav_dataset1.json",
@@ -53,6 +58,7 @@ def valid_output_json():
 def valid_output_csv():
     path = Path("data/valid/output")
     filenames = {
+        "data.csv":"heal_dd_from_csv_data_dataset1.csv",
         "sas7bdat":"heal_dd_from_sas7bdat_with_sas7bcat.csv",#TODO: reduce data so easier to manage and test
         "dta":"heal_dd_from_stata_dta_dataset1.csv",
         "sav":"heal_dd_from_spss_sav_dataset1.csv",
