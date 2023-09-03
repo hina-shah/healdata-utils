93144bed92845e29aaed16e84884c267309063dd
Michael Kranz <mbkranz@gmail.com>
2023-09-03 08:38:21 -0500

    bugfixes to tests

--- tests/test_conversion.py
+++ tests/test_conversion.py
@@ -3,6 +3,8 @@ from pathlib import Path
 from healdata_utils.conversion import convert_to_vlmd
 import json
 
+from conftest import compare_vlmd_tmp_to_output
+
 def test_convert_to_vlmd_with_registered_formats(
     valid_input_params,valid_output_json,valid_output_csv,fields_propname):
     inputtypes = list(valid_input_params.keys())
@@ -24,11 +26,12 @@ def test_convert_to_vlmd_with_registered_formats(
         _valid_output_json = valid_output_json[inputtype]
         _valid_output_csv = valid_output_csv[inputtype]
 
-        outdir = _valid_input_params[inputtype]["output_filepath"].parent
+        outdir = _valid_input_params["output_filepath"].parent
         compare_vlmd_tmp_to_output(
             tmpdir=outdir,
-            csvoutput=_valid_output_json,
-            jsonoutput=_valid_output_csv
+            csvoutput=_valid_output_csv,
+            jsonoutput=_valid_output_json,
+            fields_propname=fields_propname
         )
 
 
