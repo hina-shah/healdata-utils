f81300c2f4eb95dd052b66fb57ead43af83196fd
Michael Kranz <mbkranz@gmail.com>
2023-09-20 14:11:57 -0500

    Update sync schemas to HEAL org and main branch

--- scripts/sync_schemas.py
+++ scripts/sync_schemas.py
@@ -1,16 +1,16 @@
 import requests
 from pathlib import Path
 import pprint
+import subprocess
+
 def copy_schemas():
     pp = pprint.PrettyPrinter(indent=2,compact=True, sort_dicts=False)
     jsonschema_url = (
-        "https://raw.githubusercontent.com/norc-heal/"
-        "heal-metadata-schemas/mbkranz/variable-lvl-dev/"
-        "variable-level-metadata-schema/schemas/jsonschema/data-dictionary.json"
+      "https://raw.githubusercontent.com/HEAL/heal-metadata-schemas/main/"
+      "variable-level-metadata-schema/schemas/jsonschema/data-dictionary.json"
     )
     csvschema_url = (
-        "https://raw.githubusercontent.com/norc-heal/heal-metadata-schemas/"
-        "mbkranz/variable-lvl-dev/"
+        "https://raw.githubusercontent.com/HEAL/heal-metadata-schemas/main/"
         "variable-level-metadata-schema/schemas/frictionless/csvtemplate/fields.json"
     )
     healjsonschema = requests.get(jsonschema_url).json()
@@ -23,6 +23,10 @@ def copy_schemas():
     py_schema_dir.joinpath("__init__.py").write_text("from .jsonschema import healjsonschema\nfrom .frictionless import healcsvschema")
     py_schema_dir.joinpath("jsonschema.py").write_text(f"healjsonschema = {healjsonschema_str}")
     py_schema_dir.joinpath("frictionless.py").write_text(f"healcsvschema = {healcsvtemplate_str}")
+    
+    for f in ["jsonschema.py","frictionless.py"]:
+        subprocess.run(["black",str(py_schema_dir.joinpath(f))])
 
+if __name__ == "__main__":
+    copy_schemas()
 
-copy_schemas()
